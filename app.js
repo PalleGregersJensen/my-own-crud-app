89ec50fadfa553dbf9c4380ec2dbce3ed18c7a4a
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-22 06:20:17 +0200

    upvote-knap tilføjet

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ function addParticpantsToArray(event) {
   const name = form.name.value;
   const age = Number(form.age.value);
   const occupation = form.select.value;
-  const newParticipant = createNewParticipant(name, age, occupation);
+  const upvote = 0;
+  const newParticipant = createNewParticipant(name, age, occupation, upvote);
   console.log(newParticipant);
   participants.push(newParticipant);
   console.log(participants);
@@ -25,11 +26,12 @@ function addParticpantsToArray(event) {
   showParticpants(participants);
 }
 
-function createNewParticipant(name, age, occupation) {
+function createNewParticipant(name, age, occupation, upvote) {
   const participant = {
     name: name,
     age: age,
     occupation: occupation,
+    upvote: upvote,
   };
   return participant;
 }
@@ -37,15 +39,18 @@ function createNewParticipant(name, age, occupation) {
 function showParticpants(participantsList) {
   document.querySelector("#participants-list").innerHTML = "";
   for (const participant of participantsList) {
-    const participantHtml = /*html*/ `<li>Name: ${participant.name} - Age: ${participant.age} - Occupation: ${participant.occupation} <button class="update-button">Update</button> <button class="delete-button">Delete</button></li>`;
+    const participantHtml = /*html*/ `<li>Name: ${participant.name} - Age: ${participant.age} - Occupation: ${participant.occupation} <button class="update-button">Update</button> <button class="delete-button">Delete</button><button class="upvote-button">Upvote</button></li>`;
     document.querySelector("#participants-list").insertAdjacentHTML("beforeend", participantHtml);
 
     const participantElement = document.querySelector("#participants-list li:last-child");
     const updateButton = participantElement.querySelector(".update-button");
     const deleteButton = participantElement.querySelector(".delete-button");
+    const upvoteButton = participantElement.querySelector(".upvote-button");
 
     updateButton.addEventListener("click", updateParticipant);
     deleteButton.addEventListener("click", deleteParticipant);
+    upvoteButton.addEventListener("click", upvoteParticipant);
+
   }
 }
 
@@ -59,4 +64,10 @@ function deleteParticipant() {
   // participants.pop();
   // console.log(participants);
   // showParticpants(participants);
+}
+
+function upvoteParticipant() {
+  console.log("upvote participant");
+  upvote++;
+  console.log(participants);
 }
